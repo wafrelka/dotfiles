6a64a98db922305a1d9809d6d17e1bbfd8fda7c1
wafrelka <wafrelka@gmail.com>
2020-09-21 02:06:18 +0900

    Fix unquoted paths in `install.sh`

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-ln -sf $(pwd)/tmux.conf.txt ~/.tmux.conf
-ln -sf $(pwd)/vimrc.txt ~/.vimrc
-ln -sf $(pwd)/zshrc.txt ~/.zshrc
-ln -sf $(pwd)/gitconfig.txt ~/.gitconfig
+ln -sf "$(pwd)/tmux.conf.txt" ~/.tmux.conf
+ln -sf "$(pwd)/vimrc.txt" ~/.vimrc
+ln -sf "$(pwd)/zshrc.txt" ~/.zshrc
+ln -sf "$(pwd)/gitconfig.txt" ~/.gitconfig
 
 mkdir -p ~/.peco
-ln -sf $(pwd)/peco.json ~/.peco/config.json
+ln -sf "$(pwd)/peco.json" ~/.peco/config.json
