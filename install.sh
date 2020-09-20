7ea896479153a8d4e3389a5a845aebe409662aef
wafrelka <wafrelka@gmail.com>
2020-09-21 02:04:30 +0900

    peco: Make 'peco' always load my config file

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -sf $(pwd)/tmux.conf.txt ~/.tmux.conf
 ln -sf $(pwd)/vimrc.txt ~/.vimrc
 ln -sf $(pwd)/zshrc.txt ~/.zshrc
 ln -sf $(pwd)/gitconfig.txt ~/.gitconfig
+
+mkdir -p ~/.peco
+ln -sf $(pwd)/peco.json ~/.peco/config.json
