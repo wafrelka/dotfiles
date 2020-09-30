43fc46eaa22bb248e4c11d49177d1bad23d51db0
wafrelka <wafrelka@gmail.com>
2020-09-30 18:13:59 +0900

    Move peco and git config files inside $XDG_CONFIG_HOME

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ln -sf "$(pwd)/tmux.conf.txt" ~/.tmux.conf
 ln -sf "$(pwd)/vimrc.txt" ~/.vimrc
 ln -sf "$(pwd)/zshrc.txt" ~/.zshrc
-ln -sf "$(pwd)/gitconfig.txt" ~/.gitconfig
 
-mkdir -p ~/.peco
-ln -sf "$(pwd)/peco.json" ~/.peco/config.json
+conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
+mkdir -p "$conf_dir/git" && ln -sf "$(pwd)/gitconfig.txt" "$conf_dir/git/config"
+mkdir -p "$conf_dir/peco" && ln -sf "$(pwd)/peco.json" "$conf_dir/peco/config.json"
