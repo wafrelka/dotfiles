af8cd4566108fe1c46d5393cebe7f331c5bb169f
wafrelka <wafrelka@gmail.com>
2020-09-30 18:19:25 +0900

    git: Add the global gitignore file

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ln -sf "$(pwd)/zshrc.txt" ~/.zshrc
 
 conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "$conf_dir/git" && ln -sf "$(pwd)/gitconfig.txt" "$conf_dir/git/config"
+mkdir -p "$conf_dir/git" && ln -sf "$(pwd)/gitignore.txt" "$conf_dir/git/ignore"
 mkdir -p "$conf_dir/peco" && ln -sf "$(pwd)/peco.json" "$conf_dir/peco/config.json"
