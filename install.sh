aa2022dc75348f1b467995ae8c58b094e0e5a226
wafrelka <wafrelka@gmail.com>
2021-08-19 19:56:26 +0900

    Make `install.sh` executable from outside of the directory

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ run () {
 	"$@"
 }
 
-run ln -sf "$(pwd)/tmux.conf.txt" ~/.tmux.conf
-run ln -sf "$(pwd)/vimrc.txt" ~/.vimrc
-run ln -sf "$(pwd)/zshrc.txt" ~/.zshrc
+here="$(cd $(dirname "$0"); pwd)"
+
+run ln -sf "$here/tmux.conf.txt" ~/.tmux.conf
+run ln -sf "$here/vimrc.txt" ~/.vimrc
+run ln -sf "$here/zshrc.txt" ~/.zshrc
 
 conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
 run mkdir -p "$conf_dir/git" && run ln -sf "$(pwd)/gitconfig.txt" "$conf_dir/git/config"
