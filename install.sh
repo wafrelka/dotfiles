a923e357819989288545c718ff866830e0515ecf
wafrelka <wafrelka@gmail.com>
2022-01-12 16:38:24 +0900

    Rename dotfiles for better editor highlighting support

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ run () {
 
 here="$(cd "$(dirname "$0")"; pwd)"
 
-run ln -sf "$here/tmux.conf.txt" ~/.tmux.conf
-run ln -sf "$here/vimrc.txt" ~/.vimrc
-run ln -sf "$here/zshrc.txt" ~/.zshrc
+run ln -sf "$here/tmux.conf" ~/.tmux.conf
+run ln -sf "$here/master.vimrc" ~/.vimrc
+run ln -sf "$here/master.zshrc" ~/.zshrc
 
 conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
-run mkdir -p "$conf_dir/git" && run ln -sf "$here/gitconfig.txt" "$conf_dir/git/config"
-run mkdir -p "$conf_dir/git" && run ln -sf "$here/gitignore.txt" "$conf_dir/git/ignore"
+run mkdir -p "$conf_dir/git" && run ln -sf "$here/master.gitconfig" "$conf_dir/git/config"
+run mkdir -p "$conf_dir/git" && run ln -sf "$here/master.gitignore" "$conf_dir/git/ignore"
 run mkdir -p "$conf_dir/peco" && run ln -sf "$here/peco.json" "$conf_dir/peco/config.json"
